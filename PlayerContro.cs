45a7eae378755965c0806c73aca0a1e3ed2d71d8
rajsarjal <80190625+rajsarjal@users.noreply.github.com>
2021-03-07 14:35:35 +0530

    Add files via upload
    this is first game 3d leanig

---
+++ PlayerContro.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerContro : MonoBehaviour
+{   Rigidbody rb;
+   public GameObject winText;
+    // Start is called before the first frame update
+   public float speed;
+   void Start()
+   {
+       speed = 10f;
+
+      // Destroy(gameObject,5f);
+       rb = GetComponent<Rigidbody>();
+
+
+   }
+    
+
+        // update is called once per frame
+         void Update()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal")*Time.deltaTime*speed;
+            float vertical = Input.GetAxisRaw("Vertical")*Time.deltaTime*speed;
+            transform.Translate(horizontal,0f,vertical);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Destroy(gameObject,3f);
+
+               // rb.AddForce(Vector3.up * 500);
+            }
+                
+            //rb.velocity = Vector3.forward * 5f;
+          if (Input.GetKeyDown(KeyCode.R))
+
+        {
+            SceneManager.LoadScene("level2");
+        }
+        
+        }
+        private void OnMouseDown()
+        {
+            Destroy(gameObject);
+            
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+
+            { 
+               // Destroy(gameObject);
+               Destroy(collision.gameObject);
+
+               winText.SetActive(true);
+
+            }
+        }
+
+    
+    };
+
+
+
